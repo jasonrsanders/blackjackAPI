2be5f2ded1b4495ef6f53231dbd7568eb0efb01c
jasonsanders <jas.sanders@gmail.com>
2017-02-22 16:41:22 -0700

    trying to make the ACE point issue work, I think I figured out the logic, I just need to execute on it now

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -6,7 +6,7 @@ angular.module('blackjack').controller('mainCtrl', function($scope, $http, black
       $scope.deck = response.data;
       var value = 0;
       $scope.deck.points = 0;
-      console.log($scope.deck);
+      // console.log($scope.deck);
       $scope.cardsValue($scope.deck.cards);
       $scope.cards = $scope.deck.cards;
       return $scope.deck;
@@ -16,7 +16,7 @@ angular.module('blackjack').controller('mainCtrl', function($scope, $http, black
   $scope.draw = function() {
     $http.get('https://deckofcardsapi.com/api/deck/' + $scope.deck.deck_id + '/draw/?count=1')
     .then(function(response) {
-      console.log(response.data.cards[0]);
+      // console.log(response.data.cards[0]);
       $scope.deck.cards.push(response.data.cards[0]);
       $scope.deck.remaining--;
       $scope.cardValue($scope.deck.cards[$scope.deck.cards.length - 1])
@@ -26,7 +26,7 @@ angular.module('blackjack').controller('mainCtrl', function($scope, $http, black
 
   $scope.cardsValue = function(cards) {
     for(var i = 0; i < cards.length; i++) {
-      console.log(parseInt(cards[i].value));
+      // console.log(parseInt(cards[i].value));
       if(cards[i].value === 'ACE') {
         if($scope.deck.points <= 10) {
           value = 11;
@@ -38,18 +38,17 @@ angular.module('blackjack').controller('mainCtrl', function($scope, $http, black
       } else {
         value = parseInt(cards[i].value);
       }
-      console.log(value);
-      if($scope.deck.points + value > 21) {
-        $scope.deck.points = 'Game over!';
-      } else if($scope.deck.points === 21) {
-        $scope.deck.points = 'You win!';
-      } else $scope.deck.points += value
-      console.log($scope.deck.points);
+      // console.log(value);
+      $scope.deck.points += value
+      // console.log($scope.deck.points);
     }
   }
 
+  //CREATE A VALUE FOR EACH CARD IN A HAND ON THE CARDS ARRAY
+  //AND RE-QUERY THE TOTAL SCORE EACH TIME A NEW CARD IS DRAWN
+
   $scope.cardValue = function(cards) {
-    console.log(parseInt(cards.value));
+    // console.log(parseInt(cards.value));
     if(cards.value === 'ACE') {
       if($scope.deck.points <= 10) {
         value = 11;
@@ -61,13 +60,19 @@ angular.module('blackjack').controller('mainCtrl', function($scope, $http, black
     } else {
       value = parseInt(cards.value);
     }
-    console.log(value);
-    if($scope.deck.points + value > 21) {
-      $scope.deck.points = 'Game over!';
-    } else if($scope.deck.points === 21) {
-      $scope.deck.points = 'You win!';
-    } else $scope.deck.points += value
-    console.log($scope.deck.points);
+    // console.log(value);
+    $scope.deck.points += value
+
+    // for(var i = 0; i < cards.length - 1; i++) {
+    //   if(cards[i].value === 'ACE') {
+    //     if($scope.deck.points > 21) {
+    //       $scope.deck.points -= 10;
+    //     } else {
+    //       value = 11;
+    //     }
+    //   }
+    // }
+    // console.log($scope.deck.points);
   }
 
 });
